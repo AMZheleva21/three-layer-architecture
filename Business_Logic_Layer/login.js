1d965ba289419686653072591decf8feeb2bbaa1
AMZheleva21 <107105698+AMZheleva21@users.noreply.github.com>
2023-12-19 22:09:34 +0200

    Update js file

--- Business_Logic_Layer/login.js
+++ Business_Logic_Layer/login.js
@@ -1,13 +1,24 @@
-function validateLogin() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-
-    // Perform basic validation (you should implement more secure validation)
-   
-    if (username === 'alex' && password === "123") {
-        document.getElementById('result').innerText = 'Login successful! 🙂';
+async function validateCredentials() {
+    const enteredUsername = document.getElementById('username').value;
+    const enteredPassword = document.getElementById('password').value;
+    
+    // Fetch credentials from the server
+    const response = await fetch('users.txt');
+    const credentialsText = await response.text();
+    
+    // Parse credentials
+    const storedCredentials = credentialsText.trim().split('\n');
+  
+    // Check if entered credentials are valid
+    const isValid = storedCredentials.some(credential => {
+      const [storedUsername, storedPassword] = credential.split(':');
+      return enteredUsername === storedUsername && enteredPassword === storedPassword;
+    });
+  
+    // Display validation result
+    if (isValid) {
+      document.getElementById('validationResult').textContent = 'Credentials are valid!';
     } else {
-        document.getElementById('result').innerText = 'Invalid username or password. Please try again.';
+      document.getElementById('validationResult').textContent = 'Invalid credentials. Please try again.';
     }
-}
-
+  }
